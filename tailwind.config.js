eab36c790620ee966523b1b8a3bed47c45ccbef9
rolccbangalore-star <rolccbangalore@gmail.com>
2025-09-03 02:17:56 +0530

    Fix PostCSS config for Tailwind on Vercel

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",  // includes all app dir files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
