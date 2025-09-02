cc12c0fe942eef71a354b9f94fbe2a9e10a5008c
rolccbangalore-star <rolccbangalore@gmail.com>
2025-09-03 02:06:22 +0530

    fix:updated file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true, // since you’re using /app directory
+  },
 };
 
 module.exports = nextConfig;
